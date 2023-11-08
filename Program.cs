395137624e4cb28973fcf4c56dd9469f920426da
dinooo <145918377+dindooo@users.noreply.github.com>
2023-11-08 08:52:34 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace drugiZadatak
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            for (i=1900; i<=2100; i++)
+            {
+                if(((i%400==0) || (i%4==0) && (i%100!=0)))
+                {
+                    Console.WriteLine(i+" je prijestupna");                  
+                }
+                else
+                {
+                    Console.WriteLine(i+" nije prijestupna");                   
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
